353ba6d9650e7cfb7256b5546c197b00c7ed3885
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-02-27 23:46:27 +0530

    Refactor

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Steps to contribute: 
+1. Make changes in the app code 
+2. Run the command 
+``npm run deploy``
+
+This will directly publish to github pages. Make sure that your code is accepted into 
+master before running the command
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo3.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className = "container">
+        <img src={logo} className="bg" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
