b96ed9c6695d804f32602585a564949dae0c9a63
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-03-18 18:43:40 +0530

    Add calendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import backgroundImage from './logo4.jpg';
-import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaEnvelope, FaLinkedin, FaGithub, FaCalendar } from 'react-icons/fa';
 
 export default function App () {
     const backgroundStyle = {
@@ -18,12 +18,13 @@ export default function App () {
     return (
         <div style={backgroundStyle}>
             <div className="overlay">
-                <h1>Hi, I'm Mayank Agarwal</h1>
-                <p>I make things work</p>
+                <h1 className={"intro"}>Hi, I'm Mayank Agarwal</h1>
+                <p className={"remark"}>Solving complex problems with ease</p>
                 <div className="social-icons">
                     <a href="mayankagarwal44442@gmail.com"><FaEnvelope /></a>
                     <a href="https://www.linkedin.com/in/mayank-agarwal44442/"><FaLinkedin /></a>
                     <a href="https://github.com/mayankagarwals"><FaGithub /></a>
+                    <a href={"https://calendly.com/mayankagarwals"}><FaCalendar/></a>
                 </div>
             </div>
         </div>
